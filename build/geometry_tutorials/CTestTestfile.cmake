1a7f5580dc0454f7492d1a9a458a8c9b160af273
Mannan Khan <mannan.khan99816@gmail.com>
2026-03-04 17:33:26 +0100

    My first ROS2 robotic arm

---
+++ build/geometry_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/diseq1/ros2_ws/src/geometry_tutorials/geometry_tutorials
+# Build directory: /home/diseq1/ros2_ws/build/geometry_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
