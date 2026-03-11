0ed593cb972a3a935be863fe75fe6f2a9b210e62
Mannan Khan <mannan.khan99816@gmail.com>
2026-03-11 13:25:44 +0100

    Clean workspace ignore build install log + working robot arm

--- build/geometry_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/diseq1/ros2_ws/src/geometry_tutorials/geometry_tutorials
-# Build directory: /home/diseq1/ros2_ws/build/geometry_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/turtlesim/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/diseq1/ros2_ws/ros_tutorials/turtlesim
-# Build directory: /home/diseq1/ros2_ws/build/turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("turtlesim__py")
